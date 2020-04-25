47a16ad0ca4cc4372cee6a4240ee8d60b3a7a302
anabneri <$git_config_user_email>
2020-04-24 23:02:02 -0300

    :bug: fix(): algorithm using a counter

--- src/com/br/algoritmos/basic/CompareTheTriples.java
+++ src/com/br/algoritmos/basic/CompareTheTriples.java
@@ -20,50 +20,44 @@ import static java.util.stream.Collectors.toList;
 
     public class CompareTheTriples {
 
-        // Complete the compareTriplets function below.
-        static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
 
-            int totalAlice = 0, totalBob = 0;
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
 
-            for (int i=0; i<3; i++) {
-                int scoreAlice = a.get(i);
-                int scoreBob = b.get(i);
-                if (totalAlice != totalBob) {
-                    int temp = scoreAlice > scoreBob ? totalAlice++ : totalBob++;
-                }
+        int A = 0, B = 0;
+        for (int i = 0; i < a.size(); i++){
+            if (a.get(i) > b.get(i)){
+                A++;
+            } else if (a.get(i) < b.get(i)){
+                B++;
             }
-
-            List<Integer> result =  new ArrayList<>();
-            result.add(totalAlice);
-            result.add(totalBob);
-            return result;
-
         }
+        return Arrays.asList(A, B);
+    }
 
-        public static void main(String[] args) throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-            List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                    .map(Integer::parseInt)
-                    .collect(toList());
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
-            List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                    .map(Integer::parseInt)
-                    .collect(toList());
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
-            List<Integer> result = compareTriplets(a, b);
+        List<Integer> result = compareTriplets(a, b);
 
-            bufferedWriter.write(
-                    result.stream()
-                            .map(Object::toString)
-                            .collect(joining(" "))
-                            + "\n"
-            );
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
 
-            bufferedReader.close();
-            bufferedWriter.close();
-        }
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
     }
 
 
