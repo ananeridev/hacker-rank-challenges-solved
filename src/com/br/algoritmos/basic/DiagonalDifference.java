3b1457089655f868b012ed032e3ee5b33cffa8c4
anabneri <$git_config_user_email>
2020-04-25 17:44:14 -0300

    :bug: fix(): class statement

--- src/com/br/algoritmos/basic/DiagonalDifference.java
+++ src/com/br/algoritmos/basic/DiagonalDifference.java
@@ -36,7 +36,7 @@ class Result {
         // across the diagonals
         return Math.abs(d1 - d2);
     }
-    public class DiagonalDifference {
+    public static class DiagonalDifference {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
